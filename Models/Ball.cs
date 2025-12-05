38e82365d0df49045272bea611678f55a2f0b2c2
Nikita Ermakov <dikost1inst@gmail.com>
2025-12-05 22:04:34 +0700

    Возвращен стандартный радиус мяча (0.5)
    Изменен радиус мяча обратно с 2.0 на 0.5 для правильной физики и коллизий

--- Models/Ball.cs
+++ Models/Ball.cs
@@ -53,7 +53,7 @@ namespace Arkanoid.Models
         {
             Position = new Vector2(40, 25);
             Velocity = new Vector2(0, -1);
-            Radius = 2f;
+            Radius = 0.5f;
             Speed = 8f; // Уменьшена скорость для лучшей играбельности
             IsActive = false;
         }
